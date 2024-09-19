8dcad626d77bf90eea0a1ae6b45a9008db8942d0
Robin Long <r.long@lancaster.ac.uk>
2024-09-19 22:18:22 +0100

    test

--- graph.py
+++ graph.py
@@ -1,29 +1,42 @@
 from pyvis.network import Network
 
 projects = {
-    'FDRI': {
-        'id': 'xjxjsjh-1001-2020',
-        'name': 'FDRI',
-        'outputs' : ['pipelines', 'data lakes'],
-        'people': ['Robin', 'Faiza', 'Joe']
+    'Project_A': {
+        'id': 'A',
+        'name': 'project a',
+        'outputs' : ['method_a', 'method_b', 'method_c'],
+        'people': ['rse_1', 'rse_2', 'dev_1']
     },
-    'boost': {
-        'id': 'xjsjsjcjc-1002-2024',
-        'name': 'Research Data Cloud',
-        'outputs' : ['pipelines', 'building blocks'],
-        'people' : ['Robin', 'Faiza', 'Dominic']
+    'Project_B': {
+        'id': 'B',
+        'name': 'project b',
+        'outputs' : ['method_b', 'method_c'],
+        'people' : ['rse_1', 'dev_1', 'dev_2']
+    },
+    'Project_C': {
+        'id': 'C',
+        'name': 'project c',
+        'outputs' : ['method_c', 'method_e', 'method_f', 'method_g'],
+        'people' : ['rse_3', 'dev_2', 'dev_3']
+    },
+    'Project_D': {
+        'id': 'D',
+        'name': 'project c',
+        'outputs' : ['method_c', 'method_b', 'method_f', 'method_a'],
+        'people' : ['rse_4', 'dev_1', 'dev_3']
     }
 }
 
-G = Network(notebook=False, select_menu=True)
+G = Network(notebook=False, height='1000px', width='100%')#, select_menu=True)
 
 for project in projects:
-    G.add_node(project, size=10, label=project, title=projects[project]['name'], group='projects')
+    G.add_node(project, size=10, label=project, title=projects[project]['name'], group='projects', shape='ellipse')
     for output in projects[project]['outputs']:
-        G.add_node(output, size=5, label=output, group='outputs')
+        G.add_node(output, size=5, label=output, group='outputs', shape='ellipse')
         G.add_edge(project, output, weight=2, color='#009900')
     for person in projects[project]['people']:
-        G.add_node(person, size=5, label=person, group='people')
+        G.add_node(person, size=5, label=person, group='people', shape='ellipse')
         G.add_edge(project, person, weight=1, color='#990000')
-G.show_buttons(filter_=['groups'])
-G.show('dri.html', notebook=False)
+G
+#G.show_buttons(filter_=['groups'])
+G.show('index.html', notebook=False)
